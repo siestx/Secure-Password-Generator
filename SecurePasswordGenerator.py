b7d6070b1a75408eec0d458e78583c9feb487929
zenperr <69914990+Radquu@users.noreply.github.com>
2021-09-23 22:48:13 +0300

    Secure Password Generator v1.4
    # SecurePasswordGenerator
    It's a open source Secure Password Generator to help with very secure passwords.
    
    v1.4 Change log : 
    Now it saves the passwords on a folder, Now it asks you how many letters you want the password to have, for what to be and how many and soon a cool print! ( Thank you very much Raluvy95 for the help )
    
    v1.3 Change log :
    Now it opens a CMD with the password,
     The file is smaller,
     It now generates from default a 32 digit password ( you can change it if you need )
    
    v1.2 Change log :
    Updated the generated passwords from 15 characters to 16 characters
    , updated the print
    
    v1.1 Change log :
    Added a welcome message.
    
    v1.0 Change log:
    
    Created the Generator and made it working.

--- SecurePasswordGenerator.py
+++ SecurePasswordGenerator.py
@@ -1,13 +1,36 @@
-print('Thanks for using this tool! Made with love by zenperr and helped by Raphiel and Gam3rr.')
-
 import random
+from time import sleep
+print('Thanks for using this tool! Made with love by zenperr and helped by Raphiel, Gam3rr and Raluvy95.\nPlease press the key enter to generate more passwords!\n\n')
+
 randomShit = "!@#$%^&*"
 low = "avcdefghijklmnopqrstuvwxyz"
 high = low.upper()
 digit = "0123456789"
-a = ""
+
+times = input("how many passwords do you want? ")
+times = int(times)
+
+length = input("How many lengths do you want to generate? ")
+length = int(length)
+
+typepwd = input("For what do you need these passwords? ")
+if typepwd == '\n':
+    typepwd = "untitled"
+
+typepwd = f" {typepwd} "
+
+print("\nHere's your generated password!")
 combined = low + high + digit + randomShit
-for i in range(32):
-    a += random.choice(combined)
-print(a)
-input()
+
+with open("passwords.txt", "a") as f:
+    f.write(typepwd.center(40, "=") + "\n")
+
+for i in range(times):
+    a = ""
+    reason = type
+    for i in range(length):
+        a += random.choice(combined)
+    with open("passwords.txt", "a") as f:
+        f.write(a + "\n")
+    print(a)
+    sleep(0.5)
